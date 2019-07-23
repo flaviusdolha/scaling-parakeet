43ccaad72644acde45df705766c36c9ba0896159
Flavius Dolha <flaviusd15@gmail.com>
2019-07-23 13:57:50 +0300

    Add route for current post

--- Public/routes/posts.js
+++ Public/routes/posts.js
@@ -9,4 +9,12 @@ router.get('/', (request, response) => {
     response.send('Posts route');
 });
 
+/*
+ * Gets the current post that is running.
+ * This is a heavily used api route and should be very secured.
+ */
+router.get('/current', (request, response) => {
+    response.send('Current post');
+});
+
 module.exports = router;
