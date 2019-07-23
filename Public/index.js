54daa73bd3f9c2023dd12ab0f2656113162fb821
Flavius Dolha <flaviusd15@gmail.com>
2019-07-23 13:29:08 +0300

    Refactor index.js final variables and add support for static files

--- Public/index.js
+++ Public/index.js
@@ -5,6 +5,10 @@ const posts = require('./routes/posts');
 const app = express();
 const PORT = process.env.PORT = 3000;
 
+//
+const CLIENT_PUBLIC = path.join(__dirname, '../Client/public');
+const CLIENT_PUBLIC_INDEX_HTML = path.join(__dirname, '../Client/public', 'index.html');
+
 // Middlewares
 app.use('/api/posts/', posts);
 
@@ -12,8 +16,9 @@ app.use('/api/posts/', posts);
  * Handles the default URL page.
  */
 app.get('/', (request, response) => {
-    response.sendFile(path.join(__dirname, '../Client/public', 'index.html'));
+    response.sendFile(CLIENT_PUBLIC_INDEX_HTML);
 });
+app.use('/', express.static(CLIENT_PUBLIC));
 
 /*
  * The app listens for the specified PORT bellow.
