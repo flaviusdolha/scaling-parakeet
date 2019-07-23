f125090df117790e4b1d4cbb2b594106baf6d0c7
Flavius Dolha <flaviusd15@gmail.com>
2019-07-23 12:45:33 +0300

    Add comments on index.js

--- Public/index.js
+++ Public/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 
+/*
+ * Handles the default URL page.
+ */
 app.get('/', (request, response) => {
     response.send('Hello, world!');
 });
 
+/*
+ * The app listens for the specified PORT bellow.
+ */
 app.listen(3000);
