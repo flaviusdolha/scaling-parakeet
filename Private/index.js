2c2af3b1f809dd3d98f051538cdfa9f74b6fb38e
Flavius Dolha <flaviusd15@gmail.com>
2019-07-24 17:37:48 +0300

    Fix wrong placed routers for posts

--- Private/index.js
+++ Private/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose')
+const posts = require('./routes/posts');
 
 const app = express();
 const PORT = process.env.PORT = 3000;
+
+// Connects to the Database Server.
+mongoose.connect('mongodb://localhost/main', { useNewUrlParser: true })
+        .then(() => console.log('Connected to MongoDB...'))
+        .catch(e => console.log(e));
+
+// Middlewares
+app.use(express.json());
+app.use('/api/posts/', posts);
 
 /*
  * The app listens for the specified PORT bellow.
--- Public/routes/posts.js
+++ Public/routes/posts.js
@@ -1,55 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const router = express.Router();
-
-/*
- * Defines how should a post be structured.
- * Every post must have title, content, author and creationDate.
- * The others fields are optional, but it would be good to be.
- */
-const postSchema = mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    content: {
-        type: Object,
-        required: true
-    },
-    author: {
-        type: String,
-        required: true
-    },
-    creationDate: {
-        type: Date,
-        required: true,
-        default: Date.now
-    },
-    publicationDate: Date,
-    likes: {
-        type: Number,
-        default: 0
-    },
-    comments: Object,
-    tags: [String]
-});
-const Post = new mongoose.model('Post', postSchema);
-
-/*
- * Creates a new post in the database.
- * This is not a public api, only authorized users can acces it.
- * 
- * Get the post request -> Transform it into JS Object -> Validate the input -> Save the post into database.
- */
-router.post('/', (request, response) => {
-    const post = getPostAsObject(request.body);
-    const result = validatePost(post);
-    if (result.isValid) {
-        savePost(post, response);
-    } else {
-        response.send(result.error);
-    }
-});
 
 /*
  * Gets the current post that is running.
@@ -58,46 +8,5 @@ router.post('/', (request, response) => {
 router.get('/current', (request, response) => {
     response.send('Current post');
 });
-
-// Returns the object from the JSON request.
-function getPostAsObject(requestBody) {
-    const { title, content, author, creationDate, publicationDate, likes, comments, tags } = requestBody;
-    const post = new Post({
-        title,
-        content,
-        author,
-        creationDate,
-        publicationDate,
-        likes,
-        comments,
-        tags
-    })
-    return post;
-}
-
-// Validates each input of the post to match the requirements defined in the schema.
-function validatePost(post) {
-    try {
-        post.validate();
-        return {
-            isValid: true,
-        }
-    } catch (e) {
-        return {
-            isValid: false,
-            error: e.message
-        }
-    }
-}
-
-// Saves the post into the database.
-function savePost(post, response) {
-    try {
-        post.save()
-        response.send(post);
-    } catch (e) {
-        response.send(e.message);
-    }
-}
 
 module.exports = router;
