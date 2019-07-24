3c5f8324edfcbe8b8fafd261576b03a45a09fcca
Flavius Dolha <flaviusd15@gmail.com>
2019-07-24 12:10:32 +0300

    Define post schema

--- Public/routes/posts.js
+++ Public/routes/posts.js
@@ -1,5 +1,39 @@
-const app = require('express');
-const router = app.Router();
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+
+/*
+ * Defines how should a post be structured.
+ * Every post must have title, content, author and creationDate.
+ * The others fields are optional, but it would be good to be.
+ */
+const postSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: Object,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    creationDate: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    publicationDate: Date,
+    likes: {
+        type: Number,
+        default: 0
+    },
+    comments: Object,
+    tags: [String]
+});
+const Post = new mongoose.model('Post', postSchema);
 
 /*
  * Gets the current post that is running.
