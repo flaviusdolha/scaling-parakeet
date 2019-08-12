f021724a2b68b73a36c25ba76d449bdc6377359f
Flavius Dolha <flaviusd15@gmail.com>
2019-08-12 11:13:22 +0300

    Create User schema

---
+++ Database/schemas/User.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+
+/*
+ * Defines how a user should be.
+ * Every user must have and email, registrationDate and firstName - lastName.
+ */
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 5,
+        maxlength: 255,
+        trim: true
+    },
+    verifiedEmail: {
+        type: Boolean,
+        default: false
+    },
+    registrationDate: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 255,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 255,
+        trim: true
+    },
+    birthday: {
+        type: Date,
+    },
+    gender: {
+        type: String,
+        enum: ['male', 'female', 'unspecified'],
+        default: 'unspecified'
+    },
+    location: {
+        type: String,
+    }
+});
+
+// Sets the property of every update function so that it validates before saving into the database.
+// It does return every time the updated user.
+userSchema.pre('findOneAndUpdate', function (next) ) {
+  this.options.runValidators = true;
+  this.options.new = true;
+  this.options.useFindAndModify = false;
+  next();
+});
+
+const User = new mongoose.model('User', userSchema);
+
+module.exports = User;
