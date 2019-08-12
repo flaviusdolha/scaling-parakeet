2c4f70edb3d60c6bc5d0795359c4aab65aa39d99
Flavius Dolha <flaviusd15@gmail.com>
2019-08-12 11:39:41 +0300

    Update json package for npm

--- Database/index.js
+++ Database/index.js
@@ -1,9 +1,11 @@
 const Post = require('./schemas/Post');
+const User = require('./schemas/User');
 const functions = require('./utils/functions');
 const sanitizer = require('./utils/sanitizer');
 
 module.exports = {
     Post,
+    User,
     functions,
     sanitizer
 };
