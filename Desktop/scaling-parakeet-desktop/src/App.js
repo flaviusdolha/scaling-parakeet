a7ae8326a07b579d47e756f292a8b6bf009794d0
Flavius Dolha <flaviusd15@gmail.com>
2019-08-12 10:41:38 +0300

    Add Bootstrap

--- Desktop/scaling-parakeet-desktop/src/App.js
+++ Desktop/scaling-parakeet-desktop/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello World!</h1>
+      <h1 className="h1">Hello World!</h1>
     );
   }
 }
